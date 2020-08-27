21bdaf5ec53a3c1f4103dcc6517f10e6e3ab3ed0
hosam <hosamelmalt@gmail.com>
2020-08-27 06:31:13 +0200

    Initial Commit

---
+++ Design Banking App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Design Banking App
+//
+//  Created by hosam on 8/27/20.
+//  Copyright © 2020 hosam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
